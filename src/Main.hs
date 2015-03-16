490900937c7f878d689f3cf5a0b479ba2c5ff47b
Diogo Biazus <diogob@gmail.com>
2015-03-16 12:32:58 -0400

    Adds functions to identify cases where we should fix the database

--- src/Main.hs
+++ src/Main.hs
@@ -32,15 +32,26 @@ verifyTransactionsWith verifyFunction page = do
        []   -> putStrLn "Finish verification"
        _ -> verifyTransactionsWith verifyFunction (page + 1)
 
+shouldUpdateDb _ Nothing = True
+shouldUpdateDb transaction (Just dbRecord) = (dbState == "confirmed" && pagarmeState /= "paid") 
+                                             || ((dbState == "refunded" || dbState == "requested_refund") && pagarmeState /= "refunded")
+  where SqlByteString dbState = dbRecord ! "state"
+        String pagarmeState = transaction ! "status"
+
 main :: IO ()
 main = do
   con <- connectPostgreSQL ""
   select <- prepare con "SELECT * FROM contributions WHERE payment_id = ?;"
   let 
+    resolveConflicts transaction dbRecord =
+      if shouldUpdateDb transaction dbRecord
+         then putStrLn "Should update"
+         else putStrLn "LGTM"
+
     verifyInDB transaction = do
                  _ <- execute select [jsonToSql (transaction ! "id")]
                  result <- fetchRowMap select
-                 print result
+                 resolveConflicts transaction result
                  putStrLn $ "Transaction " ++ show (transaction ! "id") ++ " verified"
     verifyTransactions = verifyTransactionsWith verifyInDB
   verifyTransactions 1
