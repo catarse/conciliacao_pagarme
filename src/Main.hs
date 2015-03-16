03f46e8f1d2015654795b4898d0cdd7f9a2bdc46
Diogo Biazus <diogob@gmail.com>
2015-03-16 17:21:58 -0400

    Now we try to find a transaction in the database in record the results in a database table

--- src/Main.hs
+++ src/Main.hs
@@ -4,13 +4,16 @@ module Main where
 
 import Network.Pagarme
 import Network.Wreq
-import Data.Map
+import Data.HashMap.Strict ((!), member)
+import qualified Data.Map as M
 
 import Database.HDBC 
 import Database.HDBC.PostgreSQL
 
 import Data.Scientific
 import Data.Aeson
+
+type DbResult = M.Map String SqlValue
 
 usingKey :: Options -> Options
 usingKey = withApiKey ""
@@ -19,12 +22,12 @@ jsonToSql :: Value -> SqlValue
 jsonToSql value = (toSql . show . coefficient) number
   where Number number = value
 
-getConfirmedTransactionsPage :: Int -> IO [JSONMap]
+getConfirmedTransactionsPage :: Int -> IO [Object]
 getConfirmedTransactionsPage page = do
   putStrLn $ "Fetching page " ++ show page
-  getTransactions $ (byPage page . byConfirmedStatus . usingKey) defaults
+  getTransactions $ (byPage page . usingKey) defaults
 
-verifyTransactionsWith :: (JSONMap -> IO ()) -> Int -> IO ()
+verifyTransactionsWith :: (Object -> IO ()) -> Int -> IO ()
 verifyTransactionsWith verifyFunction page = do
   toVerify <- getConfirmedTransactionsPage page
   _ <- mapM_ verifyFunction toVerify
@@ -32,20 +35,67 @@ verifyTransactionsWith verifyFunction page = do
        []   -> putStrLn "Finish verification"
        _ -> verifyTransactionsWith verifyFunction (page + 1)
 
-shouldUpdateDb _ Nothing = True
+shouldUpdateDb transaction Nothing = pagarmeState /= "waiting_payment"
+  where String pagarmeState = transaction ! "status"
 shouldUpdateDb transaction (Just dbRecord) = (dbState == "confirmed" && pagarmeState /= "paid") 
                                              || ((dbState == "refunded" || dbState == "requested_refund") && pagarmeState /= "refunded")
-  where SqlByteString dbState = dbRecord ! "state"
+  where SqlByteString dbState = dbRecord M.! "state"
         String pagarmeState = transaction ! "status"
 
 main :: IO ()
 main = do
   con <- connectPostgreSQL ""
   select <- prepare con "SELECT * FROM contributions WHERE payment_id = ?;"
+  selectByKey <- prepare con "SELECT * FROM contributions WHERE key = ?;"
+  selectByCustomer <- prepare con "SELECT * FROM contributions WHERE value::int = ? AND payer_email = ?;"
+
   let 
+    returnHint transaction id = return (show id, (show . coefficient) paymentId, show pagarmeState)
+      where String pagarmeState = transaction ! "status"
+            Number paymentId = transaction ! "id"
+
+    findUpdateHintWithMetadata key transaction = do
+      _ <- execute selectByKey [toSql key]
+      result <- fetchRowMap selectByKey
+      case result of
+           Just record -> putStrLn "Found by key!!!" >> returnHint transaction (record M.! "id")
+           Nothing -> putStrLn ("Missing key " ++ show key) >> findUpdateHintWithCustomer transaction
+      
+
+    findUpdateHintWithCustomer transaction = do 
+      let Number value = transaction ! "amount"
+          v = coefficient $ value / 100
+          Object customer = transaction ! "customer"
+          String email = customer ! "email"
+      _ <- execute selectByCustomer [nToSql v, toSql email]
+      result <- fetchRowMap selectByCustomer
+      case result of
+           Just record -> putStrLn "Found by customer!!!" >> returnHint transaction (record M.! "id")
+           Nothing -> putStrLn ("Missing key " ++ show email ++ " value " ++ show v) >> returnHint transaction "Missgin key"
+
+    findUpdateHint :: Object -> Maybe DbResult -> IO (String, String, String)
+    findUpdateHint transaction Nothing = if member "metadata" transaction
+                                            then let Object metadata = transaction ! "metadata" :: Value
+                                                  in if member "key" metadata
+                                                        then let String key =  (metadata ! "key")
+                                                              in findUpdateHintWithMetadata key transaction
+                                                        else findUpdateHintWithCustomer transaction
+                                            else findUpdateHintWithCustomer transaction
+    findUpdateHint transaction (Just dbRecord) = return (show id, show paymentId, show pagarmeState)
+      where String pagarmeState = transaction ! "status"
+            String paymentId = transaction ! "id"
+            SqlInteger id = dbRecord M.! "id"
+
+    insertUpdateHint :: Object -> Maybe DbResult -> IO ()
+    insertUpdateHint transaction dbRecord = do
+      (contributionId, paymentId, state) <- findUpdateHint transaction dbRecord
+      run con "INSERT INTO temp.contributions_to_fix (contribution_id, payment_id, pagarme_state) VALUES (?, ?, ?)" (map toSql [contributionId, paymentId, state])
+      commit con
+
+    resolveConflicts :: Object -> Maybe DbResult -> IO ()
     resolveConflicts transaction dbRecord =
       if shouldUpdateDb transaction dbRecord
-         then putStrLn "Should update"
+         then insertUpdateHint transaction dbRecord
          else putStrLn "LGTM"
 
     verifyInDB transaction = do
@@ -53,6 +103,8 @@ main = do
                  result <- fetchRowMap select
                  resolveConflicts transaction result
                  putStrLn $ "Transaction " ++ show (transaction ! "id") ++ " verified"
+
     verifyTransactions = verifyTransactionsWith verifyInDB
+
   verifyTransactions 1
   disconnect con
